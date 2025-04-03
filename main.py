e4f478824472ae43ef8d9d1151af5445271fa472
privetin <81558906+privetin@users.noreply.github.com>
2025-04-03 22:06:13 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from stdict!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ stdict.py
@@ -0,0 +1,5 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("stdict")
+
+SEARCH_API_URL = "https://stdict.korean.go.kr/api/search.do"
